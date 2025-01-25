a84f9fabaf5518aeeb2013ea43fa0003984c8503
beadidd <57589034+FlameProfi@users.noreply.github.com>
2025-01-26 00:49:11 +0300

    added bcrypt to pass commit.

--- course/Constants.cs
+++ course/Constants.cs
@@ -7,9 +7,9 @@ public class Constants
 {
     public const string SecretKey = "{16A542C5-CB78-4057-963D-1D90DE97769A}";
 
-    public const string Iss = "http://localhost:5214";
+    public const string Iss = "http://localhost:5011";
 
-    public const string Aud = "http://localhost:5214";
+    public const string Aud = "http://localhost:5011";
 
     public static SymmetricSecurityKey SymmetricSecurityKey => new(Encoding.UTF8.GetBytes(SecretKey));
 }
